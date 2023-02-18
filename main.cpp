29d279a1c0ec0336705b4fa5261bf47ef1ccf4f0
accTest1122 <125455366+accTest1122@users.noreply.github.com>
2023-02-18 15:30:03 +0300

    Added cpp file

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int key = 3;
+	string str;
+	cin >> str;
+    int newChar;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (97 >= int(str[i]) <= 122)
+		{
+			newChar = 97 + (int(str[i]) + key) % 97;
+		};
+
+		cout << char(newChar);
+	}
+}
